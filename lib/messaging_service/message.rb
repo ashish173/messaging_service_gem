7b188b1f5d23815677ba28f208e0ad984bcb61b0
Ashish Singh <aitashish173@gmail.com>
2014-12-06 03:56:07 +0530

    Exposed endpoints for 1. send_message 2. get_conversation 3. get_message_from_conversation 4. get_all_conversation 5. create_message pushing final version of gem for PRUG meetup talk, Lets :rocket: :bangbang:

--- lib/messaging_service/message.rb
+++ lib/messaging_service/message.rb
@@ -10,25 +10,55 @@ module MessagingService
         'sender_id' => sender_id,
         'message' => message
       }
+
       # All these fields are necessary add condition to validate that
       # all three fields are present.
       Message.new.handle_request("songs", :post, body: data)
     end
 
-    # recieves messages from the Messaing service
-    # think if we need this?
-    def self.recieve_message
+    # fetches a conversation b/w 2 users
+    # if there is no conversation it returns a new conversation
+    # id with
+    def self.get_conversation(recipient_id: nil, sender_id: nil, conversation_params: nil, conversation_id: nil)
       
+      data = {
+        'recipient_id' => recipient_id,
+        'sender_id' => sender_id,
+        'conversation_params' => conversation_params,
+        # conversation_id is used when we only want to fetch a conversation
+        # using its id
+        'conversation_id' => conversation_id
+      }
+      data.delete_if {|k, v| v.blank? }
+
+      Message.new.handle_request("conversations", :post, body: data)
     end
 
-    # fetches a conversation b/w 2 users
-    def self.get_conversation
-      # accepts 2 params s_id and r_id
+    def self.get_messages_from_conversation(conversation_id: nil)
+      
+      data = {
+        'conversation_id' => conversation_id
+      }
+      
+      # Message.new.handle_request("messages", :post, body: data)
+      Message.new.handle_request("conversations/#{conversation_id}/messages", :get, body: data)
     end
 
     # fetches all conversations of a user
     def self.get_all_conversation
       # accepts 1 param s_id
+    end
+    
+    def self.create_message(conversation_id: nil, message_params: nil, user_id: nil)
+      
+      data = {
+        'message_params' => message_params,
+        'conversation_id' => conversation_id,
+        'user_id' => user_id
+      }
+      
+      # /conversations/:conversation_id/messages/new
+      Message.new.handle_request("conversations/#{conversation_id}/messages/new", :get, body: data)
     end
   end
 end
